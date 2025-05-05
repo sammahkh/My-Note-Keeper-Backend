2dbec3753b2c893ba8e7877fe8dc0c0f22026902
Samah Bani Odeh <samahkhalid096@gmail.com>
2025-05-05 17:23:33 +0200

    Enable CORS

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(express.json());  
 
+const cors = require('cors');
+app.use(cors());
+
 const Note = require("./models/Note");
 
 mongoose.connect(process.env.MONGO_URI, {
